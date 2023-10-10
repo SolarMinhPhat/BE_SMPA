225453715281d325a07e3416aaddf2b78afa1f4a
uocneuocday <83950328+uocnguyenngoc3010@users.noreply.github.com>
2023-10-10 15:09:44 +0700

    update code

--- SolarMP/Models/solarMPContext.cs
+++ SolarMP/Models/solarMPContext.cs
@@ -39,7 +39,7 @@ namespace SolarMP.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=LAPTOP-8LC85HGU\\SQLEXPRESS;Initial Catalog=solarMP;Persist Security Info=True;User ID=sa;Password=12");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-9R59LGC\\SQLEXPRESS03;Initial Catalog=solarMP;User ID=sa;Password=1234567890");
             }
         }
 
